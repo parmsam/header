0947609f34017ae7bc8dbd79ddc6284d78adf7a3
Sam Parm <40672605+parmsam@users.noreply.github.com>
2019-05-12 22:42:25 -0400

    example header for r programs

---
+++ header.R
@@ -0,0 +1,28 @@
+# Created by NAME on YEAR-MT-DY
+# Validated by NAME on YEAR-MT-DY
+# Purpose: 
+# Production Schedule (if program is to be run on a regular basis):
+# Limitations and Warnings: 
+# Data Source Info: 
+# Program Derived From "other_program_name.R" (if applicable)
+# Program Flow Description (high level review of the steps of the program) ; ----
+#  1) Define key parameters
+#  X) Clean up. 
+
+# Leave mark at beginning of program log see when the run started (if needed)
+#print("###############################################")
+#paste("Run started at", Sys.time())
+#print("###############################################")
+
+# 1) Define key constants, formats, etc. ; ----
+#### PROJECT CODE GOES HERE ####
+
+# X) Clean up. ----
+# Delete all objects that were created to clean up environment
+# Uncomment when ready to run
+#rm(list=ls())
+
+# Leave mark at end of program to see when the run ended (if needed)
+#print("###############################################")
+#paste("Run ended at", Sys.time())
+#print("###############################################")
