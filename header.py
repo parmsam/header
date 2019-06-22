8fc35af5edc627b1d0facd8bc39ce41a2689ffb6
Sam Parm <40672605+parmsam@users.noreply.github.com>
2019-06-22 17:59:43 -0500

    Update header.py

--- header.py
+++ header.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
 
+"""
+Filename: header.py
+Author: Sam
+Creation Date: 06/22/2019
+Modification Date: 06/22/2019
+Python Version: 3.7.0
+Purpose: creating a header for python scripts
+Notes: Could develop script to create python files with header included with user input
+"""
